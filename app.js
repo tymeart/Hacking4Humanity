79d76ed042e99855d4dc036f98d0677375edf1e8
Lucas <lucasbkellner@gmail.com>
2017-07-08 16:09:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// log in using facebook, twitter, google, linkedin?
+
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+}).listen(9615);
+
+// localhost:9615/oauth?response_type=token&client_id=666&redirect_uri=xxx&scope=email
+// log in as anonymous - mask all identifiable datas
+
+// privacy policy, what we will look at, what we will use.
---
+++ mailChimpAPIKey.js
@@ -0,0 +1,2 @@
+
+const mailChimpKey = c597fce51e2b7405c35267a264a6db1b-us16;
