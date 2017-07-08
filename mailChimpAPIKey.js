e2176519c5c2e80b2755ab61c9fd88b4616a9a5d
lucask42 <lucasbkellner@gmail.com>
2017-07-08 16:34:38 -0700

    setup dependencies and hide API keys!

--- mailChimpAPIKey.js
+++
@@ -1,2 +0,0 @@
-
-const mailChimpKey = c597fce51e2b7405c35267a264a6db1b-us16;
---
+++ scripts/meetupAPI.js
@@ -0,0 +1,23 @@
+const request = require(‘request’)
+
+var exports = module.exports = {}
+let url = "https://api.meetup.com/"
+let token = window.location.hash.split(“access_token=“)[1].split(“&”)[0]
+let numResults = ‘3’
+
+exports.findGroupByTerms = (terms) => {
+ terms = Array.isArray(terms) ? terms.join() : terms
+
+request(`${url}find/groups?page=${numResults}&text=${terms}&access_token=${token}`, (error, response, body) => {
+   if (!error && response.statusCode == 200) {
+     let groups = JSON.parse(body)
+     // console.log(data)
+     // DO WHATEVER WE WANT WITH THE DATA HERE
+     groups.forEach((group) => {
+       console.log(`NAME: ${group.name}\nLINK: ${group.link}\n`)
+     })
+   } else {
+     console.log(response.statusCode)
+   }
+ })
+}
