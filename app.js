1e01d4633bb45b000bd74f76e600fec92fc2ea49
lucask42 <lucasbkellner@gmail.com>
2017-07-08 22:54:45 -0700

    update

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ app.get('/', function (req, res) {
 })
 
 app.get('/authd.ejs', function (req, res) {
-  let token = req.originalUrl.split("access_token=")[1].split("&")[0]
-  var result = search.findGroupByTerms('ruby')
+  //let token = req.originalUrl.split("access_token=")[1].split("&")[0]
+  let token = window.location.hash.split("access_token=")[1].split("&")[0]
+  var result = search.findGroupByTerms('RUBY')
 
   res.render('authd', {result})
 })
